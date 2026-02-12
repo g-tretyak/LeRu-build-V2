ccb94818038d8ed176faf0c11cca337454d03d54
g_tretyak <137423551+g-tretyak@users.noreply.github.com>
2026-02-13 00:54:17 +0300

    build 2

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-LeRu-v.01.12.02.2026.23:00";
+const cacheName = "DefaultCompany-LeRu-v.01.13.02.2026.00:52";
 const contentToCache = [
-    "Build/e2824c871ef418552c2f7ad1b3e732d8.loader.js",
+    "Build/36bd8e4e1972efa26ffb976c585335ba.loader.js",
     "Build/478ecd4ea8c438fde32ed1c2b0090080.framework.js",
-    "Build/316a487ddc3f000599be0c064afb3750.data",
-    "Build/647617d99b5666d2c2a72e4264c81c7c.wasm",
+    "Build/7d0af68c1eff92de26563fb71731877b.data",
+    "Build/d7e2978ae09ab5a3644de936a3180557.wasm",
     "TemplateData/style.css"
 
 ];
